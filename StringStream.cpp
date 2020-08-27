25e5f6e9d912532def3bf72dae2f451c9ef2f0c2
walidbelaabed <unknown.dza@gmail.com>
2020-08-27 01:44:21 +0100

    stringStream problem solution

---
+++ StringStream.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <vector>
+#include <sstream>
+#include <string>
+using namespace std;
+vector<int> parseInts(string str) {
+	vector<char>p;
+	vector<int>parser;
+	
+	for (size_t i=0;i<str.length();i++) {
+		if (str[i]==','){ 
+			string conca = "";
+			for (auto i:p)
+			{
+				conca+=i;
+				p.pop_back();
+			}
+				
+			    stringstream geek(conca);
+					int x;
+					geek >> x; 
+					parser.push_back(x);
+			
+		}else{
+				if(i == str.size()-1){
+							p.push_back(str[i]);
+							string conca = "";
+							for (auto i:p){
+									conca+=i;
+									p.pop_back();
+							}
+					stringstream geek(conca);
+					int x;
+					geek >> x; 
+					parser.push_back(x);
+
+				}else 
+					p.push_back(str[i]);		
+		} 
+}
+	return parser;
+}
+int main(){
+
+	  ios_base::sync_with_stdio(false);
+  	cin.tie(NULL);
+		#ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+  	#endif
+ string str;
+    cin >> str;
+    vector<int> integers = parseInts(str);
+    for(int i = 0; i < integers.size(); i++) {
+        cout << integers[i] << "\n";
+    }
+    return 0;
+}
